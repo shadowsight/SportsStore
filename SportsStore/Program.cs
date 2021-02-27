e2689e618506ab8be4017c42893901ac21ce0e0c
shadowsight <30534190+shadowsight@users.noreply.github.com>
2021-02-27 07:13:11 -0600

    Save 2-27-21

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -8,9 +8,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace SportsStore {
-    public class Program {
-        public static void Main(string[] args) {
+namespace SportsStore
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
             BuildWebHost(args).Run();
         }
 
@@ -22,3 +25,5 @@ namespace SportsStore {
                 .Build();
     }
 }
+
+// Git Changes test.
